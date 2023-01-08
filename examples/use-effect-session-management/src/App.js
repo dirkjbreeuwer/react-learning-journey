6aa4540ae73e42c99a4edf346f5240d0f989f62b
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-08 10:15:56 +0000

    Refactor setFormIsValid using useEffect

--- examples/use-effect-session-management/src/App.js
+++ examples/use-effect-session-management/src/App.js
@@ -19,8 +19,6 @@ function App() {
 
 
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
     localStorage.setItem("isLoggedIn","1")
   };
