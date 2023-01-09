d63aa232c6bb372398b02c66115448a09d3ceab6
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-09 06:30:44 +0000

    Refactor form reducer using useEffect

--- examples/use-reducer-form-validation/src/components/Login/Login.js
+++ examples/use-reducer-form-validation/src/components/Login/Login.js
@@ -4,37 +4,41 @@ import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
-// Goal combine value and validity of email state into one using useReducer
-
-// In React, a reducer is a function that takes in the current state of an application and an action, 
-// and returns a new state. 
-
 const emailReducer = (state, action) => {
-  if (action.type === "USER_INPUT"  ) {
-    return {value : action.val, isValid : action.val.includes("@")} 
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
   }
-  if (action.type === "INPUT_BLUR") {
-    return {value: state.value, isValid: state.value.includes("@")}
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@') };
   }
-  return {value : '', isValid : false}
-
-}
+  return { value: '', isValid: false };
+};
 
 const passwordReducer = (state, action) => {
-  if (action.type === "USER_INPUT") {
-    return {value : action.val, isValid : action.val.trim().length > 6} 
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
   }
-  if (action.type === "INPUT_BLUR") {
-    return {value : state.value, isValid : state.value.trim().length > 6} 
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
   }
-  return {value : '', isValid : false}
-}
+  return { value: '', isValid: false };
+};
 
 const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {value : '', isValid : false})
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: false})
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '',
+    isValid: null,
+  });
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -44,29 +48,41 @@ const Login = (props) => {
     };
   }, []);
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-    dispatchEmail({type: 'USER_INPUT', val: event.target.value})
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
 
-    setFormIsValid(
-      emailState.value.includes('@') && passwordState.isValid
-    );
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
-    dispatchPassword({type: 'USER_INPUT', val: event.target.value})
+    dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
 
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-    );
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
-    dispatchEmail({type: 'INPUT_BLUR'})
+    dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
-    dispatchPassword({type: 'INPUT_BLUR'})
+    dispatchPassword({ type: 'INPUT_BLUR' });
   };
 
   const submitHandler = (event) => {
