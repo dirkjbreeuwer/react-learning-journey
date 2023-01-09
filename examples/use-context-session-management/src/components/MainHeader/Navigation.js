04a3532f12ff32334a9c0751952811c922c59d1a
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-09 07:24:14 +0000

    Remove unused props from navigation

--- examples/use-context-session-management/src/components/MainHeader/Navigation.js
+++ examples/use-context-session-management/src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import React, {useContext} from 'react';
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext)
 
         return (
