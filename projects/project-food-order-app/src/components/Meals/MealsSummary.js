782ad417e912d039de730b39fa234c9b95c900fe
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-11 06:41:31 +0000

    feat: add meal summary and meal items

---
+++ projects/project-food-order-app/src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food. Delivered to You.</h2>
+      <p>Choose your favourite meal from our broad selection...</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
