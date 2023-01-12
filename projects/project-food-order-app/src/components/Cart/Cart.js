1ba76db803a0f48a5d78baee429ad285dd616714
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-12 07:10:21 +0000

    feat: add cart modal - WIP

---
+++ projects/project-food-order-app/src/components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        { id: "c1", name: "sushi", amount: 2, price: 12.99 },
+        { id: "c2", name: "pizza", amount: 1, price: 10.99 },
+      ].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      <div>{cartItems}</div>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>$35.52</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
