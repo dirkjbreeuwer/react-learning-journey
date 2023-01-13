d5bb5e79d3fc4a4fcacc79193d02a57eae068431
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-13 05:30:24 +0000

    feat: add cart provider boilerplate

---
+++ projects/project-food-order-app/src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import CartContext from './cart-cotext'
+
+const CartProvider = (props) => {
+    const addItemToCartHandler = item => {};
+
+    const removeItemFromCartHandler = item => {};
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler,
+    }
+
+  return (
+    <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartProvider
