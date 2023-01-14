d21af336f1efd196c8e6684ff691f66b480f14e9
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-14 15:35:10 +0000

    feat: Allow items to be removed from cart

--- projects/project-food-order-app/src/App.js
+++ projects/project-food-order-app/src/App.js
@@ -1,30 +1,30 @@
-import { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import { useState } from 'react';
 
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  const [cartVisible, setCartVisible] = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartVisible(true)
-  }
+    setCartIsShown(true);
+  };
+
   const hideCartHandler = () => {
-    setCartVisible(false)
-  }
+    setCartIsShown(false);
+  };
 
   return (
     <CartProvider>
-      {cartVisible && (<Cart onHideCart={hideCartHandler}/>)}
-      <Header onShowCart={showCartHandler}/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </CartProvider>
   );
 }
 
 export default App;
-
